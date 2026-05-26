117f7ce833a07a985209ecb37e6d93b266884f30
JEVA <desoftware3@gmail.com>
2026-05-25 20:41:36 -0600

    variables

---
+++ variable.py
@@ -0,0 +1,11 @@
+# Guardamos datos en variables
+nombre = "Eva"
+edad = 25
+es_docente = True
+altura = 1.65
+
+# Mostramos los valores
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("¿Es docente?", es_docente)
+print("Altura:", altura, "metros")
