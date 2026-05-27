08f6b5652f2fd6dc4c1675cf5a1882e2fc4af939
JEVA <desoftware3@gmail.com>
2026-05-26 20:24:28 -0600

    Create entradadedatos.py

---
+++ entradadedatos.py
@@ -0,0 +1,8 @@
+# Ejemplo de entrada de datos en Python
+
+# Pedimos datos al usuario
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tienes? "))
+
+# Mostramos un mensaje personalizado
+print("Hola", nombre, "tienes", edad, "años.")
